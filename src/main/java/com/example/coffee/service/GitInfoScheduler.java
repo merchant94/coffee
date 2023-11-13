5af7f7761088d7bd320677f913f399f9c84a4d9d
KIMSANGIN <tkddls0322@gmail.com>
2023-11-13 15:57:25 +0900

    refactor: Move @EnableScheduling to GitInfoScheduler

--- src/main/java/com/example/coffee/service/GitInfoScheduler.java
+++ src/main/java/com/example/coffee/service/GitInfoScheduler.java
@@ -7,6 +7,7 @@ import com.example.coffee.repository.GitInfoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@EnableScheduling
 public class GitInfoScheduler {
 
     @Autowired
