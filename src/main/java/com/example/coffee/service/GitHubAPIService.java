1b5cfb5e55d3db74c753c13106252026eda5f286
KIMSANGIN <tkddls0322@gmail.com>
2023-11-13 15:54:27 +0900

    feat: Externalize GitHub API properties

--- src/main/java/com/example/coffee/service/GitHubAPIService.java
+++ src/main/java/com/example/coffee/service/GitHubAPIService.java
@@ -3,6 +3,7 @@ package com.example.coffee.service;
 import com.example.coffee.exception.CustomException;
 import com.example.coffee.exception.NotFoundException;
 import com.example.coffee.domain.GitHubReleaseInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -11,15 +12,19 @@ import reactor.core.publisher.Mono;
 @Service
 public class GitHubAPIService {
     private final WebClient webClient;
+    @Value("${github.api.url}")
+    private String githubApiUrl;
+    @Value("${github.api.release.endpoint}")
+    private String githubApiReleaseEndpoint;
 
     public GitHubAPIService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://api.github.com").build();
+        this.webClient = webClientBuilder.baseUrl(githubApiUrl).build();
     }
 
     public Mono<GitHubReleaseInfo> getReleaseInfo(String owner, String repo){
         return webClient
                 .get()
-                .uri("/repos/{owner}/{repo}/releases/latest",owner,repo)
+                .uri(githubApiReleaseEndpoint,owner,repo)
                 .retrieve()
                 .bodyToMono(GitHubReleaseInfo.class)
                 .onErrorResume(WebClientResponseException.class, e -> {
